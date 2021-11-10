6188d6c78bc61099d8e24398afb43865936f4038
2000031490sisir <89300898+2000031490sisir@users.noreply.github.com>
2021-11-10 15:43:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import loginpage from './allpages/Loginpage';
+import Loginpage from './allpages/Loginpage';
+import Home from './allpages/Home';
+import Contact from './allpages/contact';
+import signuppage from './allpages/Signuppage';
+import Signuppage from './allpages/Signuppage';
+import Header from './allpages/Header';
+import Lgproducts from './allpages/companies/Lgproducts';
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    </div>
+  );
+}
+
+export default App;
